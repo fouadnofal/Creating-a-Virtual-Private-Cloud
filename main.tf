48a77ff6633eef56974a804db61a77ce2e377644
Fouad <fouadnofal@outlook.com>
2025-03-21 20:46:19 -0700

    Create main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
 provider "aws" {}
+
+resource "aws_vpc" "lab-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "Public-subnet" {
+    vpc_id = aws_vpc.lab-vpc.id
+    cidr_block = "10.0.0.0/24"
+  
+
+}
+
+resource "aws_subnet" "private-subnet" {
+    vpc_id = aws_vpc.lab-vpc.id
+    cidr_block = "10.0.2.0/23"
+  
+}
